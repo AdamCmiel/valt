f3f4e40b504494c3e659cb81651ea99aaaae33f5
Adam Cmiel <adam@famo.us>
2014-12-07 17:38:50 -0800

    create message authentication covered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        User.find session[:user_id]
+        User.find session[:user_id] if session[:user_id]
     end
 
     def authenticate_user!
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -4,15 +4,26 @@ class MessagesControllerTest < ActionController::TestCase
     setup do
         @adam = User.create name: "Adam", password: "foo"
         @steven = User.create name: "Steven", password: "bar"
+        @message = {to: @steven.name, body: "Hi Steven"}
+        @bad_message = {to: "Alice", body: "Hi Alice"}
     end
 
-    test "can create a message" do
-        message = {to: @steven.name, body: "Hi Steven"}
-        post :create, message, {user_id: @adam.id}
-        assert_equal assigns(:message).body, message[:body]
+    test "can send a message to a user" do
+        post :create, @message, {user_id: @adam.id}
+        assert_equal assigns(:message).body, @message[:body]
         assert_equal assigns(:message).sender, @adam
         assert_equal assigns(:message).recipient, @steven
         assert_response :success
+    end
+
+    test "can't send a message to a non-user" do
+        post :create, @bad_message, {user_id: @adam.id}
+        assert_response :bad_request
+    end
+
+    test "a non-authenticated-user can't send a message" do
+        post :create, @message
+        assert_response :unauthorized
     end
 
     test "user can find his messages" do
